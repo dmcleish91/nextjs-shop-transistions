bf8f95137f13e7d51ffc093ef9d0ff21e70736bb
Dwight McLeish Jr <dmcleishjr@gmail.com>
2022-12-07 22:51:10 -0500

    fix: added meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import '../../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
 	const router = useRouter();
 	return (
 		<AnimatePresence mode='wait'>
+			<Head>
+				<meta http-equiv='Content-Security-Policy' content='upgrade-insecure-requests'></meta>
+			</Head>
 			<motion.div
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
